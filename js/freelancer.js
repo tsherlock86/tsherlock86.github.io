d68f378b89e822d1718babcbfa7387a823b527eb
tsherlock86 <tadsherlock@gmail.com>
2015-10-18 16:55:28 -0400

    video is finally fixed

--- js/freelancer.js
+++ js/freelancer.js
@@ -63,16 +63,20 @@ $(document).ready(function(){
   autoPlayYouTubeModal();
 })
 
-//FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
-function autoPlayYouTubeModal(){
-  var trigger = $("body").find('[data-toggle="modal"]');
-  trigger.click(function() {
-    var theModal = $(this).data( "target" ),
-    videoSRC = $(this).attr( "data-theVideo" ),
-    videoSRCauto = videoSRC+"?autoplay=1" ;
-    $(theModal+' iframe').attr('src', videoSRCauto);
-    $(theModal+' button.close').click(function () {
-        $(theModal+' iframe').attr('src', videoSRC);
-    });
-  });
-}
+// //FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
+// function autoPlayYouTubeModal(){
+//   var trigger = $("body").find('[data-toggle="modal"]');
+//   trigger.click(function() {
+//     var theModal = $(this).data( "target" ),
+//     videoSRC = $(this).attr( "data-theVideo" ),
+//     videoSRCauto = videoSRC+"?autoplay=1" ;
+//     $(theModal+' iframe').attr('src', videoSRCauto);
+//     $(theModal+' button.close').click(function () {
+//         $(theModal+' iframe').attr('src', videoSRC);
+//     });
+//   });
+// }
+
+$("#videoModal").on('hidden.bs.modal', function (e) {
+    $("#videoModal iframe").attr("src", $("#videoModal iframe").attr("src"));
+});
